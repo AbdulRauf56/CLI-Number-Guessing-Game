39cb0910cb67df6135d063ec5fc605cd075d7de6
Abdul Rauf <arauf8970@gmail.com>
2024-03-24 22:36:29 +0500

    Adding a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to Abdul Rauf - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your Guess Number(Number Limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulagion ! You guess a correct Number.");
+}
+else {
+    console.log("Sorry, you guess a wrong Number.");
+}
